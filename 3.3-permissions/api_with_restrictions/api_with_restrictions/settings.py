72985192ae64e489c4cbbd2cb95f70901bd318c6
vladimir <m9043887765@gmail.com>
2023-09-17 18:48:19 +0500

    commit

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
-from .sec import user_name, password
+from .secc import user_name, password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
