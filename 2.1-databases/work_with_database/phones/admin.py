8c55c7376c8e545980183c721a8d766615cab012
vladimir <m9043887765@gmail.com>
2023-10-06 13:15:39 +0500

    fff

--- 2.1-databases/work_with_database/phones/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from phones.models import Phone
-
-@admin.register(Phone)
-class PhoneAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'image', 'release_date', 'lte_exists')
--- 3.3-permissions/api_with_restrictions/advertisements/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from advertisements.models import Advertisement
-
-admin.site.register(Advertisement)
